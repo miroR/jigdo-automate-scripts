706522e693155fa2586d7f57d0b6eb6ec49f7298
Miroslav Rovis <miro.rovis@croatiafidelis.hr>
2014-05-13 09:02:20 +0200

    Preps for the goal set earlier (all scripts deduced from SUMS file)
    Just the skeleton for future writing the lines, as many as there are Jigdo
    DVDs, as found out from the SHA256SUMS

---
+++ jigdo-dl_01-dLo-SUMS.sh
@@ -0,0 +1,8 @@
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/MD5SUMS
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/MD5SUMS.sign
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA1SUMS
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA1SUMS.sign
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA256SUMS
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA256SUMS.sign
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA512SUMS
+wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA512SUMS.sign
--- jigdo-dl_03-dLo-SUMS.sh
+++
@@ -1,4 +0,0 @@
-wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/MD5SUMS
-wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA1SUMS
-wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA256SUMS
-wget -nc http://cdimage.debian.org/cdimage/weekly-builds/amd64/jigdo-dvd/SHA512SUMS
