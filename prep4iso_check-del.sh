f7b4759cbc3f3d2b5638e0c18eb05cbd2b4b71f8
Miroslav Rovis <miro.rovis@croatiafidelis.hr>
2014-05-13 09:02:20 +0200

    Preps for the goal set earlier (all scripts deduced from SUMS file)
    Just the skeleton for future writing the lines, as many as there are Jigdo
    DVDs, as found out from the SHA256SUMS

--- prep4iso_check-del.sh
+++ prep4iso_check-del.sh
@@ -23,11 +23,13 @@
 
 cat SHA256SUMS | wc -l > linesall
 linesall=`cat linesall` ;
-#echo $linesall
-#read FAKE ;
+echo $linesall
+read FAKE ;
+echo "The lines2last will be -9, at least as long as we're more than 10 DVDs"
+echo "+and less than 20"
 lines2last=`echo $linesall-9|bc`
-#echo $lines2last ;
-#read FAKE ;
+echo $lines2last ;
+read FAKE ;
 
 echo "This script will hold correctly as long as the number of Debian jigdo"
 echo "testing ISO files remains under 20, which is still a while longer :-) "
@@ -35,27 +37,74 @@ echo "...But it's an amateur script anyway. Does the work though."
 
 cat SHA256SUMS | head -1  > SHA256SUMS_PART1
 
-#cat SHA256SUMS | tail -`echo $linesall-$lines2last-1|bc`
-#read FAKE ;
+cat SHA256SUMS | tail -`echo $linesall-$lines2last-1|bc`
+read FAKE ;
 cat SHA256SUMS | tail -`echo $linesall-$lines2last-1|bc` > SHA256SUMS_PART2
 
-#cat SHA256SUMS | tail -`echo $linesall-1|bc`|head -$lines2last
-#read FAKE ;
+cat SHA256SUMS | tail -`echo $linesall-1|bc`|head -$lines2last
+read FAKE ;
 cat SHA256SUMS | tail -`echo $linesall-1|bc`|head -$lines2last > SHA256SUMS_PART3
-#cat SHA256SUMS_PART?
-#read FAKE ;
+cat SHA256SUMS_PART?
+read FAKE ;
 cat SHA256SUMS_PART? > SHA256SUMS_r
 echo "All this script to just sort the order of the SHA256SUMS file to natural order."
 echo "Disappointing! But I don't know better."
 echo ;
 echo "Did you notice the SHA256SUMS_r text file that is now ordered?"
 echo "We now have to create the text files that are now traditionally"
-echo "--in the parody mood only-- in the iso_check-del_NN.sh called: "
+echo "--in the parody mood only-- in the iso_check-del.sh called: "
 echo "SHA256SUMS-bare and debian-DVD-list, and which are nothing much, but"
 echo "only the first and the third column of the SHA256SUMS_r file,"
 
 cat SHA256SUMS_r | cut -d' ' -f1 > SHA256SUMS-bare
 cat SHA256SUMS_r | cut -d' ' -f3 > debian-DVD-list
+
+echo "Should be fine for running the iso_check-del.sh, but we need to get the"
+echo "jigdo files and the templates first, so we need create scripts"
+echo "jigdo-dl_0[234]*.sh from jigdo-dl_0[234]*-4sed.sh one-liner"
+echo "prep-templates"
+
+echo "Jigdo DVDs are now digitally signed. Let's get the key (innocuous"
+echo "error/delay if you already have the key):"
+
+gpg --recv-key 0x09EA8AC3
+
+echo "We'll settle for the SHA256 sums which are strong enough (MD5 isn't),"
+echo "and we'll verify only them first:"
+
+gpg --verify SHA256SUMS.sign
+
+echo "Pls. refer elsewhere if you have difficulties understanding PGP"
+echo "If you get: Good signature from \"Debian Testing CDs Automatic Signing"
+echo "Key and the right fingerprint:"
+echo "F41D 3034 2F35 4669 5F65  C669 4246 8F40 09EA 8AC3"
+echo "go on, else, don't. Quit with Ctrl-C and investigate."
+
+echo "What we next do, is, do a somewhat complicated thing to just get the"
+echo "basically number of DVDs, and to exactly just get the numbers for each"
+echo "DVD in one line, for our primitive sed text manipulation (did I already"
+echo "say how these scripts were bloated... but do the work?)"
+
+cat SHA256SUMS_r | cut -d'.' | cut -d'-' -f5 >> thnumbers.txt
+
+cat thnumbers.txt
+read FAKE ;
+
+for i in `cat thnumbers.txt` ; do sed "s/thnumber/$i/g" jigdo-dl_02-jigdo-templ-t-4sed.sh >> jigdo-dl_02-jigdo-templ-t.sh ; done ;
+chmod 755 jigdo-dl_02-jigdo-templ-t.sh
+
+for i in `cat thnumbers.txt` ; do sed "s/thnumber/$i/g" jigdo-dl_03-mnt-t-4sed.sh >> jigdo-dl_03-mnt-t.sh ; done ;
+chmod 755 jigdo-dl_03-mnt-t.sh
+
+for i in `cat thnumbers.txt` ; do sed "s/thnumber/$i/g" jigdo-dl_04-dlo-t-4sed.sh >> jigdo-dl_04-dlo-t.sh ; done ;
+chmod 755 jigdo-dl_04-dlo-t.sh
+
+cat jigdo-dl_02-jigdo-templ-t.sh
+read FAKE ;
+cat jigdo-dl_03-mnt-t.sh
+read FAKE ;
+cat jigdo-dl_04-dlo-t.sh
+read FAKE ;
 
 echo "There! Now we can run the script, which anyway called this little"
 echo "clumsy amateur helper script which is now done and out. Bye!"
